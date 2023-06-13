f5ee80f59a145d96e70ee34ce6b1960788790dbb
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-06-13 08:05:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var isDate = function (input) {
   //   write your code here
+	
+  const date = new Date(input);
+  return isValidDate(date);
+}
+
+function isValidDate(date) {
+  return date instanceof Date && !isNaN(date);
+
+
 };
 
 // Do not change the code below.
